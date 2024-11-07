029183dda14b43a371e0e716ee6d7b01977c26f6
minhngo <minhcorn.2@gmail.com>
2024-11-07 21:30:28 +0700

    cấu hình dự án

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>React App</h1>
+        </div>
+    );
+}
+
+export default App;
