1492d5a96a438bd760c3f9107fedb17071ca7de9
minhngo <minhcorn.2@gmail.com>
2024-11-28 03:21:41 +0700

    cập nhật phần homePage

---
+++ src/pages/HomePage/components/Carousel/index.js
@@ -0,0 +1,129 @@
+// Component Carousel
+// * dùng chung để hiện các danh sách theo ô nằm ngang như danh sách dành cho bạn, nhạc thịnh hành,...
+// ---------------------------------------------------------------------------------------------------
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import aptBanner from '~/assets/images/643564536.jpg';
+
+function Carousel({ slides }) {
+    // config carousel (React Slick)
+    const settings = {
+        dots: false,
+        arrows: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4.5,
+        slidesToScroll: 2,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    // slidesToShow: 1,
+                    // slidesToScroll: 1,
+                    slidesToShow: 2.5,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+        ],
+    };
+    // data mẫu
+    // nếu data không có thì gán bằng mảng rỗng, hoặc bằng mảng data mẫu
+    slides || (slides = []);
+
+    return (
+        <>
+            <div className="carousel">
+                <Slider {...settings}>
+                    {slides.map((slide) => (
+                        <>
+                            <div
+                                className="carouselItem"
+                                style={{
+                                    display: 'grid',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <img
+                                    src={slide.img}
+                                    className="slide-image"
+                                    style={{
+                                        height: '150px',
+                                        width: '150px',
+                                        objectFit: 'cover',
+                                        objectPosition: 'center',
+                                        // border: '.5px solid rgba(18, 18, 18, 0.8)',
+                                    }}
+                                />
+                                <div
+                                    className="info"
+                                    style={{
+                                        display: 'grid',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <span
+                                        className="info1"
+                                        style={{
+                                            color: '#ffffff',
+                                            paddingBottom: '1px',
+                                            paddingTop: '5px',
+                                            fontSize: '14px',
+                                            fontWeight: '400',
+                                            width: '115px',
+                                            display: '-webkit-box',
+                                            WebkitBoxOrient: 'vertical',
+                                            WebkitLineClamp: '1',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                        }}
+                                    >
+                                        {slide.info1}
+                                    </span>
+                                    <span
+                                        className="info2"
+                                        style={{
+                                            color: 'rgba(119, 119, 119, 0.6666666667)',
+                                            fontSize: '14px',
+                                            fontWeight: '300',
+                                            width: '115px',
+                                            display: '-webkit-box',
+                                            WebkitBoxOrient: 'vertical',
+                                            WebkitLineClamp: '1',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                        }}
+                                    >
+                                        {slide.info2}
+                                    </span>
+                                </div>
+                            </div>
+                        </>
+                    ))}
+                </Slider>
+            </div>
+        </>
+    );
+}
+
+export default Carousel;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,12 @@
-import logo from '~/assets/images/logoNoBackground.png';
 import { useEffect, useRef } from 'react';
 import aptBanner from '~/assets/images/643564536.jpg';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { VscChevronLeft, VscChevronRight } from 'react-icons/vsc';
+// import Component
+import Carousel from './components/Carousel';
+// hết import Component
 
 function HomePage() {
     // config slider (React Slick)
@@ -62,7 +64,85 @@ function HomePage() {
             img: `https://www.sportshub.com.sg/sites/default/files/2024-05/CAS2025-1200x675-hi-res.png`,
         },
     ];
-
+    // data mẫu cho carousel Dành cho bạn
+    const dataMixedForYou = [
+        {
+            img: `https://al.sndcdn.com/labs-c93bb0af-0-t500x500.jpg?q=YXJ0d29ya190eXBlOiBNRUdBX01JWAp1cm5zOiAic291bmRjbG91ZDp0cmFja3M6MTIyODEyNjc1NiIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjE1MjIxNzU5ODMiCnVybnM6ICJzb3VuZGNsb3VkOnRyYWNrczoxMTY0NjE3MTcwIgp1cm5zOiAic291bmRjbG91ZDp0cmFja3M6MTcwMDcxOTQ3OSIKdXJuczogInNvdW5kY2xvdWQ6dHJhY2tzOjE1ODY5OTcxOTEiCg%3D%3D`,
+            info1: 'Mega Mix',
+            info2: `Cảnh Hưng, Tez, naughty kid, Chànq Try Năm Ngoái, Alex`,
+        },
+        {
+            img: `https://pickasso.spotifycdn.com/image/ab67c0de0000deef/dt/v1/img/daily/4/ab6761610000e5eb4b2da0b72cab26ac518f1f0d/vi`,
+            info1: 'Daily Mix 4',
+            info2: 'Deftones',
+        },
+    ];
+    // data mẫu cho carousel Âm nhạc thịnh hành
+    const dataTrendingMusic = [
+        {
+            img: `https://static01.nyt.com/images/2024/11/22/arts/22kendrick-album/22kendrick-album-facebookJumbo.jpg`,
+            info1: 'GNX',
+            info2: 'Kendrick Lamar',
+        },
+        {
+            img: aptBanner,
+            info1: 'APT',
+            info2: 'Rose ft. Bruno Mars',
+        },
+        {
+            img: `https://mensfolio.vn/wp-content/uploads/2023/11/20231311_MENSFOLIO_GRAMMY-1.webp`,
+            info1: 'Đá tan',
+            info2: 'Ngọt',
+        },
+        {
+            img: `https://img.vietcetera.com/uploads/images/16-oct-2023/feature.jpg`,
+            info1: 'Đánh đổi',
+            info2: 'Obito ft. MCK',
+        },
+        {
+            img: `https://i.ytimg.com/vi/eW4AM1539-g/maxresdefault.jpg`,
+            info1: 'TRỞ VỀ',
+            info2: 'Wxrdie ft. JustaTee',
+        },
+        {
+            img: `https://www.sportshub.com.sg/sites/default/files/2024-05/CAS2025-1200x675-hi-res.png`,
+            info1: 'Cry',
+            info2: 'Cigarettes After Sex',
+        },
+    ];
+    // data mẫu cho carousel Nhạc Theo thể loại
+    const dataSongByGenre = [
+        {
+            img: `https://i1.sndcdn.com/artworks-405SXVNHbz8mzHNv-5Z35dw-t500x500.jpg`,
+            info1: 'Pop',
+            info2: 'Genre',
+        },
+        {
+            img: `https://i1.sndcdn.com/artworks-J5PgDX18TAcCBZMD-7HgRcw-t500x500.jpg`,
+            info1: 'Hip Hop & Rap',
+            info2: 'Genre',
+        },
+        {
+            img: `https://i1.sndcdn.com/artworks-hKhAdJqrPFA50ydp-RVWo2Q-t500x500.jpg`,
+            info1: 'Rock',
+            info2: 'Genre',
+        },
+        {
+            img: `https://i1.sndcdn.com/artworks-ITaPkyvGgrRMh6zK-9JZ0uw-t500x500.jpg`,
+            info1: 'Indie',
+            info2: 'Genre',
+        },
+        {
+            img: `https://i1.sndcdn.com/artworks-KklvPbOcNqis0t72-xKiLAw-t500x500.jpg`,
+            info1: 'R&B',
+            info2: 'Genre',
+        },
+        {
+            img: `https://i1.sndcdn.com/artworks-oyBVayO8ytBG3uUG-gwqKXg-t500x500.jpg`,
+            info1: 'Electronic',
+            info2: 'Genre',
+        },
+    ];
     // Đổi title trang
     useEffect(() => {
         document.title = 'Home | mymusic';
@@ -101,6 +181,27 @@ function HomePage() {
                 <button className="btnNextCarousel" onClick={next}>
                     <VscChevronRight />
                 </button>
+            </div>
+            {/* Phần carousel mixed for you */}
+            <div className="carouselMixedForYou">
+                <div className="title">
+                    <span>Dành cho bạn</span>
+                </div>
+                <Carousel slides={dataMixedForYou}></Carousel>
+            </div>
+            {/* Phần carousel trending music */}
+            <div className="carouselTrendingMusic">
+                <div className="title">
+                    <span>Âm nhạc thịnh hành</span>
+                </div>
+                <Carousel slides={dataTrendingMusic}></Carousel>
+            </div>
+            {/* Phần carousel nhạc theo thể loại */}
+            <div className="carouselByGenre">
+                <div className="title">
+                    <span>Theo thể loại</span>
+                </div>
+                <Carousel slides={dataSongByGenre}></Carousel>
             </div>
             <div style={{ padding: '8px' }}>
                 <h1>
