a054d98f436b18f3947b40a3ddfaf3d31f916024
minhngo <minhcorn.2@gmail.com>
2024-12-22 21:26:26 +0700

    cập nhật article, feedPage,....

--- src/pages/components/Article/index.js
+++ src/pages/components/Article/index.js
@@ -8,7 +8,7 @@ import aptBanner from '~/assets/images/643564536.jpg';
 // Hết Import Component
 
 function Article({ feed }) {
-    // config slider (React Slick)
+    // config Carousel Media (React Slick)
     let sliderRef = useRef(null);
     const next = () => {
         sliderRef.slickNext();
@@ -17,11 +17,12 @@ function Article({ feed }) {
         sliderRef.slickPrev();
     };
     const settings = {
-        dots: false,
+        dots: feed.feed.media.length > 1 ? true : false,
         arrows: false,
-        infinite: false,
+        infinite: feed.feed.media.length > 1 ? true : false,
+        draggable: feed.feed.media.length > 1 ? true : false,
         speed: 500,
-        slidesToShow: 2.5,
+        slidesToShow: 1,
         slidesToScroll: 1,
         initialSlide: 0,
         accessibility: true,
@@ -29,18 +30,20 @@ function Article({ feed }) {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true,
+                    draggable: feed.feed.media.length > 1 ? true : false,
                 },
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1.5,
+                    slidesToShow: 1,
                     slidesToScroll: 1,
                     initialSlide: 2,
+                    draggable: feed.feed.media.length > 1 ? true : false,
                 },
             },
             {
@@ -48,9 +51,10 @@ function Article({ feed }) {
                 settings: {
                     // slidesToShow: 1,
                     // slidesToScroll: 1,
-                    slidesToShow: 1.5,
+                    slidesToShow: 1,
                     slidesToScroll: 1,
                     initialSlide: 2,
+                    draggable: feed.feed.media.length > 1 ? true : false,
                 },
             },
         ],
