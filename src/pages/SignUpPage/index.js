55609e81206b140fe519a2830539ef65541392dd
minhngo <minhcorn.2@gmail.com>
2025-03-05 21:56:25 +0700

    update signUpPage,...

--- src/pages/SignUpPage/index.js
+++ src/pages/SignUpPage/index.js
@@ -1,16 +1,40 @@
 import logo from '~/assets/images/logoWhiteTransparent.png';
-import { IoLogoGoogle, IoAlertCircleOutline } from 'react-icons/io5';
+import { IoLogoGoogle, IoChevronBackSharp, IoEyeOffOutline, IoEyeOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 function SignUpPage() {
+    // State (useState)
+    const [formStep, setFormStep] = useState(0); // Mỗi step tương ứng với một thành phần khác nhau trong form
+    const [isShowPassword, setIsShowPassword] = useState(false); // Hiển thị mật khẩu
+
     const formSignUp = useForm();
-    const { register, handleSubmit, formState } = formSignUp;
+    const { register, handleSubmit, formState, watch } = formSignUp;
     const { errors } = formState;
 
-    // Check data khi submit form
+    // Handle Button Next Step
+    const handleBtnNextStep = (data) => {
+        console.log('Button Next Step clicked');
+    };
+
+    // Handle Button Show Password
+    const handleBtnShowPassword = () => {
+        var inputPassword = document.getElementById('password');
+        if (inputPassword.type === 'password') {
+            setIsShowPassword(true);
+            inputPassword.type = 'text';
+        } else {
+            setIsShowPassword(false);
+            inputPassword.type = 'password';
+        }
+    };
+
+    // Handle Submit Form Sign Up
     const onSubmit = (data) => {
         console.log('Form submitted', data);
+        // Đến bước tiếp theo
+        setFormStep(formStep + 1);
     };
 
     return (
@@ -19,43 +43,179 @@ function SignUpPage() {
                 <img className="logo" src={logo} alt="Logo mymusic" draggable="false" />
                 <form className="signUpForm" onSubmit={handleSubmit(onSubmit)} method="POST" noValidate>
                     <span className="title">Đăng ký</span>
-                    <label className="labelEmail" htmlFor="email">
-                        Địa chỉ email
-                    </label>
-                    <input
-                        className="inputEmail"
-                        id="email"
-                        name="email"
-                        type="email"
-                        placeholder="example@gmail.com"
-                        {...register('email', {
-                            required: `Vui lòng nhập email`,
-                            pattern: {
-                                // value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                                message: `Email này không hợp lệ. Hãy đảm bảo rằng email được nhập dưới dạng example@email.com`,
-                            },
-                        })}
-                    />
-                    <p className="errorMessage">{errors.email?.message}</p>
+                    {/* Step 0 */}
+                    {formStep === 0 && (
+                        <>
+                            {/* Email */}
+                            <label className="labelEmail" htmlFor="email">
+                                Địa chỉ email
+                            </label>
+                            <input
+                                className="inputEmail"
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="example@gmail.com"
+                                {...register('email', {
+                                    required: `Vui lòng nhập email`,
+                                    pattern: {
+                                        // value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                        message: `Email này không hợp lệ. Hãy đảm bảo rằng email được nhập dưới dạng example@email.com`,
+                                    },
+                                })}
+                            />
+                            <p className="errorMessage">{errors.email?.message}</p>
+                        </>
+                    )}
+                    {/* Step 1 */}
+                    {formStep === 1 && (
+                        <>
+                            <div className="stepProgressBar">
+                                <div className="stepProgressBarInner" style={{ width: '50%' }}></div>
+                            </div>
+                            <div className="stepTitle">
+                                <div className="left">
+                                    <button
+                                        className="btnReturnPreviousStep"
+                                        type="button"
+                                        onClick={() => setFormStep(formStep - 1)}
+                                    >
+                                        <IoChevronBackSharp />
+                                    </button>
+                                </div>
+                                <div className="right">
+                                    <span className="step">Bước 1/2</span>
+                                    <span>Tạo mật khẩu</span>
+                                </div>
+                            </div>
+                            {/* Mật khẩu */}
+                            <label className="labelPassword" htmlFor="password">
+                                Mật khẩu
+                            </label>
+                            <div
+                                className=""
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    position: 'relative',
+                                }}
+                            >
+                                <input
+                                    className="inputPassword"
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    placeholder="Nhập mật khẩu"
+                                    {...register('password', {
+                                        required: `Vui lòng nhập mật khẩu`,
+                                        minLength: {
+                                            value: 10,
+                                            message: `Mật khẩu phải chứa ít nhất 10 ký tự`,
+                                        },
+                                        validate: (value) => {
+                                            return (
+                                                [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/].every((pattern) =>
+                                                    pattern.test(value),
+                                                ) ||
+                                                'Mật khẩu của bạn phải bao gồm ít nhất một chữ cái viết thường, một chữ cái viết hoa, một số và một ký tự đặc biệt (ví dụ: #?!&)'
+                                            );
+                                        },
+                                    })}
+                                />
+                                <button className="btnShowPassword" type="button" onClick={handleBtnShowPassword}>
+                                    {isShowPassword ? <IoEyeOutline /> : <IoEyeOffOutline />}
+                                </button>
+                            </div>
+                            <p className="errorMessage">{errors.password?.message}</p>
+                        </>
+                    )}
+                    {/* Step 2 */}
+                    {formStep === 2 && (
+                        <>
+                            <div className="stepProgressBar">
+                                <div className="stepProgressBarInner" style={{ width: '100%' }}></div>
+                            </div>
+                            <div className="stepTitle">
+                                <div className="left">
+                                    <button
+                                        className="btnReturnPreviousStep"
+                                        type="button"
+                                        onClick={() => setFormStep(formStep - 1)}
+                                    >
+                                        <IoChevronBackSharp />
+                                    </button>
+                                </div>
+                                <div className="right">
+                                    <span className="step">Bước 2/2</span>
+                                    <span>Giới thiệu thông tin</span>
+                                </div>
+                            </div>
+                            {/* Họ và Tên */}
+                            <label className="labelName" htmlFor="name">
+                                Họ và tên
+                            </label>
+                            <input
+                                className="inputName"
+                                id="name"
+                                name="name"
+                                type="text"
+                                placeholder="Nhập họ và tên của bạn"
+                                {...register('name', {
+                                    required: `Vui lòng nhập họ và tên`,
+                                    maxLength: {
+                                        value: 500,
+                                        message: `Họ và tên không được vượt quá 500 ký tự`,
+                                    },
+                                })}
+                            />
+                            <p className="errorMessage">{errors.name?.message}</p>
+                            {/* Tên người dùng */}
+                            <label className="labelUserName" htmlFor="userName">
+                                Tên người dùng
+                            </label>
+                            <input
+                                className="inputUserName"
+                                id="userName"
+                                name="userName"
+                                type="text"
+                                placeholder="Tên này sẽ xuất hiện trên hồ sơ của bạn"
+                                {...register('userName', {
+                                    required: `Vui lòng nhập tên người dùng`,
+                                    maxLength: {
+                                        value: 500,
+                                        message: `Tên người dùng không được vượt quá 500 ký tự`,
+                                    },
+                                })}
+                            />
+                            <p className="errorMessage">{errors.userName?.message}</p>
+                        </>
+                    )}
                     <button className="btnNextStep" type="submit" id="btnNextStepID">
-                        Tiếp theo
+                        {formStep === 2 ? 'Đăng ký' : 'Tiếp theo'}
                     </button>
-                    <div className="otherRegisMethod">
-                        <div className="title">
-                            <span>hoặc</span>
-                        </div>
-                        <a href="#googleRegis" className="regisMethodGoogle">
-                            <IoLogoGoogle className="logoGoogle" />
-                            <span>Đăng ký bằng Google</span>
-                        </a>
-                    </div>
-                    <span className="toSignIn">
-                        Bạn đã có tài khoản?{' '}
-                        <Link to={`/signin`} style={{ color: 'white', fontWeight: '600' }}>
-                            Đăng nhập tại đây
-                        </Link>
-                    </span>
+                    {/* Check Data */}
+                    {/* <pre style={{ color: 'white' }}>{JSON.stringify(watch(), null, 2)}</pre> */}
+                    {/* Other Sign Up Method */}
+                    {formStep === 0 && (
+                        <>
+                            <div className="otherRegisMethod">
+                                <div className="title">
+                                    <span>hoặc</span>
+                                </div>
+                                <a href="#googleRegis" className="regisMethodGoogle">
+                                    <IoLogoGoogle className="logoGoogle" />
+                                    <span>Đăng ký bằng Google</span>
+                                </a>
+                            </div>
+                            <span className="toSignIn">
+                                Bạn đã có tài khoản?{' '}
+                                <Link to={`/signin`} style={{ color: 'white', fontWeight: '600' }}>
+                                    Đăng nhập tại đây
+                                </Link>
+                            </span>
+                        </>
+                    )}
                 </form>
             </div>
         </>
